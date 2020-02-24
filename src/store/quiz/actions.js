2ebfcb69f312e4a3f7428773ffe6d457404f97a0
Ashutosh Devkota <ashutosh.devkota@gmail.com>
2020-02-23 19:31:18 -0600

    github deploy

--- src/store/quiz/actions.js
+++ src/store/quiz/actions.js
@@ -1,10 +1,12 @@
 import { showLoadingScreen, hideLoadingScreen } from "../loading_screen/actions";
 import { LOAD_QUIZ_DATA } from "./action-types";
+import data from "../../data/quiz.json"
 
 export function loadQuizData() {
 	return async dispatch => {
 		dispatch(showLoadingScreen());
-		const data = await fetch("/data.json").then(data => data.json())
+		// if we want to fetch data from external source fetch here
+		// const data = await fetch("/citizenship-practise/data.json").then(data => data.json())
 		dispatch ({
 			type: LOAD_QUIZ_DATA,
 			data
