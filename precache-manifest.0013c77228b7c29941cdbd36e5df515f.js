2801223aeb365506775bd19aa839a621192acbea
Ashutosh Devkota <ashutosh.devkota@gmail.com>
2020-02-23 19:28:01 -0600

    Updates

--- precache-manifest.0013c77228b7c29941cdbd36e5df515f.js
+++ precache-manifest.0013c77228b7c29941cdbd36e5df515f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9242b2ad9ee478ab2dddc3a7515be412",
+    "revision": "38136bf405a28b6a25374df8b312de49",
     "url": "/citizenship-practise/index.html"
   },
   {
-    "revision": "2c4781c2ae760c452780",
+    "revision": "003fd5ab65c716b929dd",
     "url": "/citizenship-practise/static/css/main.e6e91d9a.chunk.css"
   },
   {
-    "revision": "21af8a6bbd7fdf7f1127",
-    "url": "/citizenship-practise/static/js/2.7834a065.chunk.js"
+    "revision": "b96650e4878a62a07997",
+    "url": "/citizenship-practise/static/js/2.ca4219c9.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/citizenship-practise/static/js/2.7834a065.chunk.js.LICENSE.txt"
+    "url": "/citizenship-practise/static/js/2.ca4219c9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2c4781c2ae760c452780",
-    "url": "/citizenship-practise/static/js/main.b80ed7c5.chunk.js"
+    "revision": "003fd5ab65c716b929dd",
+    "url": "/citizenship-practise/static/js/main.c8e17aac.chunk.js"
   },
   {
     "revision": "7c873dcbd551fc958af7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/citizenship-practise/precache-manifest.b09b7985324f327cc93b9659469a2956.js"
+  "/citizenship-practise/precache-manifest.0013c77228b7c29941cdbd36e5df515f.js"
 );
 
 self.addEventListener('message', (event) => {
